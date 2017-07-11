48bdeb7ac4c45c4bf16f62c145f46ccedd7fb44b
Joonas Karu <joonas.karu@gmail.com>
2017-07-11 20:25:16 +0300

    muutsin koodi

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,9 +2,34 @@
 
 #include <iostream>
 
+using namespace std;
+
+
 int main() {
-	std::cout << "hello wordl";
+
+	int x = 0;
+	int y(0);
+	int z{ 0 };
+
+	int cars = 14;
+	int dept = -1000;
+	int cash = 2.32;
+	double credit = 32.32;
+	char a = 'a';
+	char* sandwich = "ham";
+	bool does_she_like_me = true;
+	auto do_i_have_pants_on = false;
+	auto blank_check = 200.00;
+	auto over_9000 = INT_MAX;
+
+	cout << over_9000 << endl;
+
+	// see rida on testiks
+
+
+	/*std::cout << "hello wordl";*/
+
+
 	return 0;
-	// panen siia niisama kommentaari
 	
 }
